442c06cf7ddc3b7d263766a88f3f7e2216af3a02
Daniel Gonzalez Torralba <dangonz3@c4r7s4.42urduliz.com>
2024-04-30 12:43:43 +0200

    segunda

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -1,35 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstadd_front_bonus.c                            :+:      :+:    :+:   */
+/*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dangonz3 <dangonz3@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/25 09:15:30 by dangonz3          #+#    #+#             */
-/*   Updated: 2024/04/29 16:37:35 by dangonz3         ###   ########.fr       */
+/*   Created: 2024/04/29 09:42:23 by dangonz3          #+#    #+#             */
+/*   Updated: 2024/04/29 18:27:02 by dangonz3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstadd_front(t_list **lst, t_list *new)
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	new->next = *lst;
-	*lst = new;
-}
+	t_list	*result;
+	t_list	*temp;
 
-/* int	main(void)
-{
-	t_list *list = NULL;
-	t_list *node1 = ft_lstnew("Nodo 1");
-	ft_lstadd_front(&list, node1);
-	t_list *node2 = ft_lstnew("Nodo 2");
-	ft_lstadd_front(&list, node2);
-	t_list *current = list;
-	while (current)
+	if (!lst || !f || !del)
+		return (NULL);
+	result = ft_lstnew(f(lst->content));
+	if (result == NULL)
+		return (NULL);
+	lst = lst->next;
+	while (lst)
 	{
-		printf("%s\n", (char *)current->content);
-		current = current->next;
+		temp = ft_lstnew(f(lst->content));
+		if (!temp)
+		{
+			ft_lstclear(&result, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&result, temp);
+		lst = lst->next;
 	}
-	return (0);
-} */
+	return (result);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: dangonz3 <dangonz3@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 13:16:24 by dangonz3          #+#    #+#             */
-/*   Updated: 2024/04/27 16:34:14 by dangonz3         ###   ########.fr       */
+/*   Updated: 2024/04/29 09:43:49 by dangonz3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,5 +64,7 @@ void			ft_lstadd_back(t_list **lst, t_list *new);
 void			ft_lstdelone(t_list *lst, void (*del)(void*));
 void			ft_lstclear(t_list **lst, void (*del)(void*));
 void			ft_lstiter(t_list *lst, void (*f)(void *));
+t_list			*ft_lstmap(t_list *lst, void *(*f)(void *),
+					void (*del)(void *));
 
 #endif
